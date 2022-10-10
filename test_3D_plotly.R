59cf2ead74eccea619d328a839e953ea0fdc79ed
Axel <axelroth2@web.de>
2022-10-10 19:48:00 +0200

    asd

--- test_3D_plotly.R
+++ test_3D_plotly.R
@@ -2,6 +2,8 @@ library(plotly)
 library(dplyr)
 library(tidyr)
 library(tidyverse)
+library(htmlwidgets)
+library(shiny)
 
 
 mrunif <- function(nr, nc, lower, upper) {
@@ -23,8 +25,8 @@ control <- list(
   s = 5, # swarm size
   c.p = 0.5, # inherit best
   c.g = 0.5, # global best
-  maxiter = 100, # iterations
-  w0 = 0.8, # starting inertia weight
+  maxiter = 20, # iterations
+  w0 = 1.2, # starting inertia weight
   wN = 0.4 # ending inertia weight
 )
 
@@ -89,32 +91,165 @@ grid <- grid %>% spread(., key = axis_2, value = fitness) %>% column_to_rownames
 
 
 
-fig <- plot_ly()
-fig <- fig %>% add_surface(
-  type = 'surface',
-  contours = list(
-    z = list(show = TRUE, start = round(min(grid)), end = round(max(grid)), size = round((max(grid)-min(grid))/10), color="grey")
-  ),
-  showscale = FALSE,
-  opacity=0.6,
-  x = rownames(grid),
-  y = colnames(grid),
-  z = grid)
-fig <- fig %>%
+fig <- plot_ly() %>%
+  add_surface(
+    type = 'surface',
+    contours = list(
+      z = list(show = TRUE, start = round(min(grid)), end = round(max(grid)), size = round((max(grid)-min(grid))/10), color="grey")
+    ),
+    showscale = FALSE,
+    opacity=0.6,
+    x = rownames(grid),
+    y = colnames(grid),
+    z = grid
+  ) %>%
   add_trace(
     data=save_X,
     x=~axis_1,
     y=~axis_2,
     z=~fitness,
     color = ~id,
+    #split = ~id,
     frame = ~iter,
     mode ='markers',
     type = 'scatter3d',
     showlegend=F,
     marker = list(color = 'red', size=4, showscale = F)
-  )
+  ) %>%
+  animation_opts(
+    redraw = T,
+    frame = 1000#,
+    # transition = 1000,
+    # easing = "linear"
+  )# %>%
+  # add_annotations(
+  #   x=1,
+  #   y=1,
+  #   z=15,
+  #   frame=0,
+  #   xref="x",
+  #   yref="y",
+  #   showarrow = TRUE,
+  #   arrowhead = 4,
+  #   arrowsize = .5,
+  #   ax = 20,
+  #   ay = -40,
+  #   text="hi"
+  # )
 fig
 
 
 
 
+
+## annotation in each frame: https://stackoverflow.com/questions/72176553/plotly-r-how-to-add-dynamic-annotation-which-belongs-to-frame
+
+
+
+fig2 <- plotly_build(fig)
+
+
+fig2$x$frames[[1]]$layout <- list(annotations = list(list(
+  type="text",
+  x=2,
+  y=2,
+  z=10,
+  frame=0,
+  xref="x",
+  yref="y",
+  showarrow = TRUE,
+  arrowhead = 4,
+  arrowsize = .5,
+  ax = 20,
+  ay = 10,
+  text="hi"
+)))
+
+fig2$x$frames[[2]]$layout <- list(annotations = list(list(
+  type="text",
+  x=3,
+  y=3,
+  z=13,
+  frame=0,
+  xref="x",
+  yref="y",
+  showarrow = TRUE,
+  arrowhead = 4,
+  arrowsize = .5,
+  ax = 20,
+  ay = 10,
+  text="hi"
+)))
+
+fig2$x$frames[[3]]$layout <- list(annotations = list(list(
+  type="text",
+  x=4,
+  y=4,
+  z=15,
+  frame=0,
+  xref="x",
+  yref="y",
+  showarrow = TRUE,
+  arrowhead = 4,
+  arrowsize = .5,
+  ax = 20,
+  ay = 10,
+  text="hi"
+)))
+
+
+# # delay einbauen in shiny: https://stackoverflow.com/questions/56855505/how-to-change-plotly-animation-speed-within-a-javascript-code-in-r
+#
+# ui <- fluidPage(
+#   actionButton("anim", "Animate"),
+#   div(style="height:100%, width:100%;", plotlyOutput("plot", width = "auto", height="auto"))
+# )
+# server <- function(input, output){
+#   output[["plot"]] <- renderPlotly({
+#     fig <- plot_ly() %>%
+#       add_surface(
+#         type = 'surface',
+#         contours = list(
+#           z = list(show = TRUE, start = round(min(grid)), end = round(max(grid)), size = round((max(grid)-min(grid))/10), color="grey")
+#         ),
+#         showscale = FALSE,
+#         opacity=0.6,
+#         x = rownames(grid),
+#         y = colnames(grid),
+#         z = grid
+#       ) %>%
+#       add_trace(
+#         data=save_X,
+#         x=~axis_1,
+#         y=~axis_2,
+#         z=~fitness,
+#         color = ~id,
+#         frame = ~iter,
+#         mode ='markers',
+#         type = 'scatter3d',
+#         showlegend=F,
+#         marker = list(color = 'red', size=4, showscale = F)
+#       ) %>%
+#       animation_button(visible = FALSE) %>%
+#       onRender("
+#           function(el,x){
+#             $('#anim').on('click', function(){
+#               Plotly.animate(el,
+#                 null,
+#                 {
+#                   transition: {
+#                     duration: 1000,
+#                     easing: 'cubic-in-out'
+#                   },
+#                   frame: {
+#                     duration: 1000
+#                   }
+#                 }
+#               );
+#             });
+#           }")
+#     fig
+#   })
+# }
+#
+# shinyApp(ui, server)
